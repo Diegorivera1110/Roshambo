bb374daf133bce434e98883b069475bb82d5b087
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-05-12 20:35:56 -0700

    set up script declarations as well as checker function

---
+++ script.js
@@ -0,0 +1,27 @@
+let [computer_score, user_score] = [0,0];
+let result_ref = document.getElementById("result");
+let choices_object = {
+    'rock' : {
+        'rock' : 'draw',
+        'scissor' : 'win',
+        'paper' : 'lose'
+    },
+    'paper' : {
+        'rock' : 'win',
+        'scissor' : 'lose',
+        'paper' : 'draw'
+    },
+    'scissor' : {
+        'rock' : 'lose',
+        'scissor' : 'draw',
+        'paper' : 'win'
+    }
+
+}
+
+function checker(input){
+    var choices = ['rock', 'paper', 'scissor'];
+    var num = Math.floor(Math.random() * 3);
+
+    
+}
